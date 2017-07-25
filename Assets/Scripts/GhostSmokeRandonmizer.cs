99ae0ce4558cd971e0e616274a3d23ca8d38078d
Peter Scheel <selech@live.dk>
2017-07-25 16:05:55 +0200

    Tilføjet animation på Grim Flamengo med tilhørende partikler, så nu ryger han!

---
+++ Assets/Scripts/GhostSmokeRandonmizer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostSmokeRandonmizer : MonoBehaviour {
+
+    private int countdown;
+    private Animator anim;
+    private ParticleSystem ps;
+
+    // Use this for initialization
+    void Start () {
+        countdown = Random.Range(90,250);
+        anim = GetComponent<Animator>();
+        ps = GetComponentInChildren<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        countdown--;
+
+        if (countdown <= 0)
+            PlaySmoke();
+	}
+
+    public void PlaySmoke()
+    {
+        countdown = Random.Range(90, 250);
+        anim.SetTrigger("Smoke");
+    }
+
+    public void StartParticle()
+    {
+        ps.Play();
+    }
+}
